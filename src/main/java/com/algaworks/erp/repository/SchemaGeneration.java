7dc6ea43396d062674b94f9e200c922e3234de76
Wender Matias da Silva <dxwender@gmail.com>
2024-09-23 11:21:22 -0300

    Componentes
    Foram apresentados os componentes do JSF

--- src/main/java/com/algaworks/erp/repository/SchemaGeneration.java
+++ src/main/java/com/algaworks/erp/repository/SchemaGeneration.java
@@ -9,19 +9,18 @@ import javax.persistence.Persistence;
 import com.algaworks.erp.model.Empresa;
 
 public class SchemaGeneration {
-	
-	public static void main(String[] args) {		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
-		
-		EntityManager em = emf.createEntityManager();
-		
-		List<Empresa> lista = em.createQuery("from Empresa", Empresa.class)
-				.getResultList();
-		
-		System.out.println(lista);
-		
-		em.close();
-		emf.close();
-	}
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
+
+        EntityManager em = emf.createEntityManager();
+
+        List<Empresa> lista = em.createQuery("from Empresa", Empresa.class).getResultList();
+
+        System.out.println(lista);
+
+        em.close();
+        emf.close();
+    }
 
 }
